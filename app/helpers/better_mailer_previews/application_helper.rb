755594837d53fbe9cbe82ec4930d059612382764
Harrison Broadbent <harrisonbroadbent@gmail.com>
2023-12-12 12:36:42 +1100

    extract helpers

--- app/helpers/better_mailer_previews/application_helper.rb
+++ app/helpers/better_mailer_previews/application_helper.rb
@@ -1,4 +1,11 @@
 module BetterMailerPreviews
   module ApplicationHelper
+    def preview_text_for_url(url)
+      return "Preview #{url.split("/")[-2...].map { |segment| segment.split("_").map(&:capitalize).join }.join(".")} →"
+    end
+
+    def preview_path_for_url(url)
+      url.split("/")[-2...].join("/").prepend("#{BetterMailerPreviews::Engine.routes.find_script_name({})}/")
+    end
   end
 end
